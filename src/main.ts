320feed173119429094c5f4d00d6e1dd7de8fbb3
Vladimir Noutbook <vovirus2@inbox.ru>
2025-05-29 12:37:26 +0300

    install inversify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
+import { Container } from 'inversify';
 import { App } from './app';
 import { ExeptionFilter } from './errrors/exeption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UsersController } from './users/users.controller';
+import { ILogger } from './logger/logger.interfase';
+import { TYPES } from './types';
+import { IExeptionFilter } from './errrors/exeption.filter.interface';
 
-async function bootstrap() {
-    const logger = new LoggerService();
-    const app = new App(
-        logger,
-        new UsersController(logger),
-        new ExeptionFilter(logger)
-    );
-    await app.init();
-}
-bootstrap();
+const container = new Container();
+container.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+container.bind<IExeptionFilter>(TYPES.IExeptionFilter).to(ExeptionFilter);
+container.bind<App>(TYPES.Application).to(App);
+container.bind<UsersController>(TYPES.UserController).to(UsersController);
+
+const app = container.get<App>(TYPES.Application);
+app.init();
+export { app, container };
