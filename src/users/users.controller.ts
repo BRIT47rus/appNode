c05711f40ac0f174854d95e11b153568ec660a74
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-03 13:03:06 +0300

    created user-entity + bcryptojs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,27 +6,28 @@ import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interfase';
 import { UserLoginDTO } from './dto/user-login.dto';
 import { UserRegisterDTO } from './dto/user-register.dto';
-
+import {User} from './user.entity'
 
 @injectable()
 export class UsersController extends BaseController {
-    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
-        super(loggerService);
-        this.bindRoutes([
-            {
-                path: '/login',
-                method: 'post',
-                func: this.login,
-            },
-            { path: '/register', method: 'post', func: this.register },
-        ]);
-    }
-    login(req: Request<{},{},UserLoginDTO>, res: Response, next: NextFunction) {
-     console.log(req.body)
-        this.ok(res, 'login');
-    }
-    register(req: Request<{},{},UserRegisterDTO>, res: Response, next: NextFunction) {
-    console.log(req.body)
-        this.ok(res, 'register');
-    }
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+			},
+			{ path: '/register', method: 'post', func: this.register },
+		]);
+	}
+	login(req: Request<{}, {}, UserLoginDTO>, res: Response, next: NextFunction) {
+		console.log(req.body);
+		this.ok(res, 'login');
+	}
+	async register({ body }: Request<{}, {}, UserRegisterDTO>, res: Response, next: NextFunction):Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok(res, newUser);
+	}
 }
