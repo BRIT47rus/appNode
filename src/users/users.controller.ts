e25b2f566c6a0a6922c1b831453fe656f0726bfb
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-05 11:44:17 +0300

    i jwt & created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import { BaseController } from '../common/base.controller';
-
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interfase';
@@ -9,12 +8,14 @@ import { UserRegisterDTO } from './dto/user-register.dto';
 import { IUserService } from './user.service.interface';
 import { HTTPError } from '../errrors/http-error.class';
 import { ValidateMiddleware } from '../common/validate.middleware';
-
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interfase';
 @injectable()
 export class UsersController extends BaseController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
 		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.IConfigService) private cofigService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -41,7 +42,9 @@ export class UsersController extends BaseController {
 		if (!result) {
 			return next(new HTTPError(401, 'ошибка авторизации'));
 		}
-		this.ok(res, {});
+		const jwt = await this.signJWT(req.body.email, this.cofigService.get('SECRET'));
+
+		this.ok(res, { jwt });
 	}
 	async register(
 		{ body }: Request<{}, {}, UserRegisterDTO>,
@@ -53,5 +56,23 @@ export class UsersController extends BaseController {
 			return next(new HTTPError(422, 'Такой пользователь уже существует'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((res, rej) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						rej(err);
+					}
+					res(token as string);
+				},
+			);
+		});
 	}
 }
