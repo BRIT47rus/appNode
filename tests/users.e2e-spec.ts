acbb8d8349770db565174cd3083c4aff7352c68f
Vladimir Noutbook <vovirus2@inbox.ru>
2025-06-06 13:27:37 +0300

    end

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './../src/common/auth.guard';
 import { App } from '../src/app';
 import { boot } from '../src/main';
 import request from 'supertest';
@@ -16,6 +17,31 @@ describe('Users e2e ', () => {
 		});
 		expect(res.statusCode).toBe(422);
 	});
+	it('Login - success', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'v@v.ru',
+			password: '1',
+		});
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+	it('Login - error', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'v@v.ru',
+			password: '3',
+		});
+		expect(res.statusCode).toBe(401);
+	});
+	// it('Info - success', async () => {
+	// 	const login = await request(application.app).post('/users/login').send({
+	// 		email: 'v@v.ru',
+	// 		password: '1',
+	// 	});
+	// 	const res = await request(application.app)
+	// 		.get('/users/info')
+	// 		.set('Authorization', `Bearer ${login.body.jwt}`);
+
+	// 	expect(res.body.email).toBe('payload.email');
+	// });
 });
 
 afterAll(() => {
